44a303edfdb706d19565514814972571b0cce645
Darwin Guzmán <dga_guz@hotmail.es>
2018-06-23 10:36:50 -0500

    Avence proyecto, typescript/javascript

---
+++ app/js/funciones.js
@@ -0,0 +1,62 @@
+console.log('q tal!');
+function dos() {
+    console.log(2);
+}
+function cambiarTitulo() {
+    var etiquetaTitulo = document.getElementById('titulo');
+    etiquetaTitulo.onmouseenter = undefined;
+    etiquetaTitulo.innerText = 'Adios mundo';
+    etiquetaTitulo.className = 'cafe';
+}
+function eliminarEventoOnMouseEnterBoton() {
+    var etiquetaBotonTitulo = document.getElementById('cambiar_titulo_button');
+    etiquetaBotonTitulo.onmouseenter = undefined;
+}
+function crearContenido() {
+    var etiquetaContenedora = document
+        .getElementById('contenido');
+    var elementoTitulo = document
+        .createElement('h5');
+    elementoTitulo.className = 'card-title';
+    elementoTitulo.textContent = 'Titulo';
+    var elementoParrafo = document
+        .createElement('p');
+    elementoParrafo.className = 'card-text';
+    elementoParrafo.textContent = 'Contenido';
+    var elementoBoton = document
+        .createElement('button');
+    elementoBoton.className = 'btn btn-primary';
+    elementoBoton.textContent = 'Accion';
+    var contenedorCardBody = document
+        .createElement('div');
+    contenedorCardBody.className = 'card-body';
+    contenedorCardBody.appendChild(elementoTitulo);
+    contenedorCardBody.appendChild(elementoParrafo);
+    contenedorCardBody.appendChild(elementoBoton);
+    var etiquetaImagen = document
+        .createElement('img');
+    etiquetaImagen.src = 'https://i.ytimg.com/vi/28P7oSkPK2M/hqdefault.jpg';
+    etiquetaImagen.className = 'card-img-top';
+    etiquetaImagen.alt = 'Imagen de mario typing';
+    var etiquetaCarta = document
+        .createElement('div');
+    etiquetaCarta.className = 'card';
+    etiquetaCarta.appendChild(etiquetaImagen);
+    etiquetaCarta.appendChild(contenedorCardBody);
+    // meter el contenido html en el contenedor
+    etiquetaContenedora.appendChild(etiquetaCarta);
+}
+function traerPost(idPost) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4
+            && this.status == 200) {
+            console.log(this.responseText);
+            var postJson = JSON.parse(this.responseText);
+            console.log(postJson);
+            console.log(postJson.userId);
+        }
+    };
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts/" + idPost, true);
+    xhttp.send();
+}
